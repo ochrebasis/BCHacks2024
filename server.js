221a6f45337721e6e4846151a1e66df4e9059f3d
ochrebasis <l.dale@bellevuecollege.edu>
2024-06-08 07:08:52 -0700

    added CORS support

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors());
+
 app.get('/api/data', (req, res) => {
     res.json({ message: 'Hello from Node.js!' });
 });
