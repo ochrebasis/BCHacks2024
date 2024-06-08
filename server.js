b490a71bca0d746a78e2d40e4f5c87703d620e04
ochrebasis <l.dale@bellevuecollege.edu>
2024-06-08 09:27:56 -0700

    pulling debug variables from backend server

--- server.js
+++ server.js
@@ -6,7 +6,13 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 
 app.get('/api/data', (req, res) => {
-    res.json({ message: 'Hello from Node.js!' });
+    res.json({ 
+        message: 'Hello from Node.js!',
+        envProcess: process.env.VERCEL,
+        envURL: process.env.VERCEL_URL,
+        envNode: process.env.NODE_ENV,
+        envVercel: process.env.VERCEL_ENV
+    });
 });
 
 app.listen(port, () => {
