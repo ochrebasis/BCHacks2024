14ef4d9b3c09af2a7a3dd06cbc83dbd846e5a774
Jeff Spacek <Jeffrey.spacek@gmail.com>
2024-06-08 10:50:26 -0700

    Create initial index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+// Endpoints
+
+
+/** NOTES 
+ * 
+ * 
+ * app.post
+ * creates new information
+ * 
+ * app.get
+ * retrieves current information
+ * 
+ * app.delete
+ * delete unwanted information
+ * 
+ * app.put
+ * updates information
+ * 
+ * 
+ * 
+ */
